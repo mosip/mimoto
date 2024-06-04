3d5c0b1d26f7064b1f7db8a364d40538f91861ef
VSIVAKALYAN <kalyanvellanki321@gmail.com>
2024-06-04 17:57:44 +0530

    [DSD-5355] Updated entrypoint.sh
    Signed-off-by: VSIVAKALYAN <kalyanvellanki321@gmail.com>

--- apitest/entrypoint.sh
+++ apitest/entrypoint.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/bin/sh
 
 ## Run automationtests
 java -jar -Dmodules="$MODULES" -Denv.user="$ENV_USER" -Denv.endpoint="$ENV_ENDPOINT" -Denv.testLevel="$ENV_TESTLEVEL" apitest-mimoto-*-jar-with-dependencies.jar;
