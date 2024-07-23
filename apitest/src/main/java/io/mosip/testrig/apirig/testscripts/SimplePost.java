202b9db163e39680bdc64a2e69df1a0942e38440
Mohanachandran S <mohanachandran.s@technoforte.co.in>
2024-07-23 13:45:13 +0530

    INJIMOB-1593 & INJIWEB-178 - Added test cases for create policy in sunbird registry and download sunbird VC
    Signed-off-by: Mohanachandran S <mohanachandran.s@technoforte.co.in>

--- apitest/src/main/java/io/mosip/testrig/apirig/testscripts/SimplePost.java
+++ apitest/src/main/java/io/mosip/testrig/apirig/testscripts/SimplePost.java
@@ -37,7 +37,6 @@ import io.restassured.response.Response;
 public class SimplePost extends AdminTestUtil implements ITest {
 	private static final Logger logger = Logger.getLogger(SimplePost.class);
 	protected String testCaseName = "";
-	public String idKeyName = null;
 	public Response response = null;
 	public boolean sendEsignetToken = false;
 	public boolean auditLogCheck = false;
@@ -67,7 +66,6 @@ public class SimplePost extends AdminTestUtil implements ITest {
 	public Object[] getTestCaseList(ITestContext context) {
 		String ymlFile = context.getCurrentXmlTest().getLocalParameters().get("ymlFile");
 		sendEsignetToken = context.getCurrentXmlTest().getLocalParameters().containsKey("sendEsignetToken");
-		idKeyName = context.getCurrentXmlTest().getLocalParameters().get("idKeyName");
 		logger.info("Started executing yml: " + ymlFile);
 		return getYmlTestData(ymlFile);
 	}
