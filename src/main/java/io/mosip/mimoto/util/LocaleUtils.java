bbed57257a42ba4337512339f6bc0bfc3f2eff8f
PuBHARGAVI <46226958+PuBHARGAVI@users.noreply.github.com>
2025-02-14 17:26:45 +0530

    [INJIWEB-1429] refactor localUtils matchLocale method to handle all use cases of inputs
    Signed-off-by: PuBHARGAVI <46226958+PuBHARGAVI@users.noreply.github.com>

--- src/main/java/io/mosip/mimoto/util/LocaleUtils.java
+++ src/main/java/io/mosip/mimoto/util/LocaleUtils.java
@@ -3,18 +3,11 @@ package io.mosip.mimoto.util;
 import java.util.Locale;
 
 public class LocaleUtils {
-    // Method to check if the localeString matches the 2-letter or 3-letter code
+    // Method to convert the input locale strings into 3-letter codes and compare
     public static boolean matchesLocale(String localeString1, String localeString2) {
-        Locale locale1 = Locale.forLanguageTag(localeString1);
+        String locale1Iso3Language = Locale.forLanguageTag(localeString1).getISO3Language(); // 3-letter code for locale1
+        String locale2Iso3Language = Locale.forLanguageTag(localeString2).getISO3Language(); // 3-letter code for locale2
 
-        // Check if localeString1 (from the object) matches either the 2-letter or 3-letter code of localeString2
-        if (localeString1.length() == 2) {
-            String iso3Language = locale1.getISO3Language(); // 3-letter code for locale1
-            return localeString1.equals(localeString2) || iso3Language.equals(localeString2);
-        } else if (localeString1.length() == 3) {
-            String iso2Language = locale1.getLanguage(); // 2-letter code for locale1
-            return localeString1.equals(localeString2) || iso2Language.equals(localeString2);
-        }
-        return false;
+        return locale1Iso3Language.equals(locale2Iso3Language);
     }
 }
