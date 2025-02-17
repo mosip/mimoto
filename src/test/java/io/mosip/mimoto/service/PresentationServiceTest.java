40dbc60f149aa6e32ca1cb60312457f22de47952
PuBHARGAVI <46226958+PuBHARGAVI@users.noreply.github.com>
2025-02-17 11:35:46 +0530

    [INJIWEB-1429] refactor the variable name which stores the exception message and add spaces between lines in few tests according to AAA rule
    Signed-off-by: PuBHARGAVI <46226958+PuBHARGAVI@users.noreply.github.com>

--- src/test/java/io/mosip/mimoto/service/PresentationServiceTest.java
+++ src/test/java/io/mosip/mimoto/service/PresentationServiceTest.java
@@ -3,10 +3,7 @@ package io.mosip.mimoto.service;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import io.mosip.mimoto.dto.mimoto.VCCredentialResponse;
-import io.mosip.mimoto.dto.openid.presentation.PresentationDefinitionDTO;
 import io.mosip.mimoto.dto.openid.presentation.PresentationRequestDTO;
-import io.mosip.mimoto.exception.ApiNotAccessibleException;
-import io.mosip.mimoto.exception.InvalidVerifierException;
 import io.mosip.mimoto.exception.VPNotCreatedException;
 import io.mosip.mimoto.service.impl.DataShareServiceImpl;
 import io.mosip.mimoto.service.impl.PresentationServiceImpl;
@@ -46,25 +43,23 @@ public class PresentationServiceTest {
         ReflectionTestUtils.setField(presentationService, "maximumResponseHeaderSize", 65536);
         when(objectMapper.writeValueAsString(any())).thenReturn("test-data");
     }
+
     @Test
     public void credentialProofMatchingWithVPRequest() throws Exception {
-
         VCCredentialResponse vcCredentialResponse = TestUtilities.getVCCredentialResponseDTO("Ed25519Signature2020");
         PresentationRequestDTO presentationRequestDTO = TestUtilities.getPresentationRequestDTO();
-
         when(dataShareService.downloadCredentialFromDataShare(eq(presentationRequestDTO))).thenReturn(vcCredentialResponse);
+        String expectedRedirectUrl = "test_redirect_uri#vp_token=dGVzdC1kYXRh&presentation_submission=test-data";
 
         String actualRedirectUrl = presentationService.authorizePresentation(TestUtilities.getPresentationRequestDTO());
-        String expectedRedirectUrl = "test_redirect_uri#vp_token=dGVzdC1kYXRh&presentation_submission=test-data";
 
         assertEquals(expectedRedirectUrl, actualRedirectUrl);
     }
 
     @Test(expected = VPNotCreatedException.class)
-    public void credentialProofMismatchWithVPRequest() throws ApiNotAccessibleException, IOException {
+    public void credentialProofMismatchWithVPRequest() throws IOException {
         VCCredentialResponse vcCredentialResponse = TestUtilities.getVCCredentialResponseDTO("RSASignature2020");
         PresentationRequestDTO presentationRequestDTO = TestUtilities.getPresentationRequestDTO();
-
         when(dataShareService.downloadCredentialFromDataShare(eq(presentationRequestDTO))).thenReturn(vcCredentialResponse);
 
         presentationService.authorizePresentation(TestUtilities.getPresentationRequestDTO());
