df647cdfd2784b9925c902188d39f23a8550c16f
PuBHARGAVI <46226958+PuBHARGAVI@users.noreply.github.com>
2025-03-04 14:17:57 +0530

    [INJIWEB-1436] add constuctor annotations on dataShareResponseDto
    Signed-off-by: PuBHARGAVI <46226958+PuBHARGAVI@users.noreply.github.com>

--- src/main/java/io/mosip/mimoto/dto/DataShareResponseDto.java
+++ src/main/java/io/mosip/mimoto/dto/DataShareResponseDto.java
@@ -2,11 +2,15 @@ package io.mosip.mimoto.dto;
 
 import java.util.List;
 
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
 @Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class DataShareResponseDto extends BaseRestResponseDTO {
 
     private static final long serialVersionUID = 1L;
