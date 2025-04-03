259c97cbbd1e6987ebb6bf091a04fa660a712523
Rakshithb1 <rakshit.b@technoforte.co.in>
2025-04-03 18:19:14 +0530

    [MOSIP-40792] updated onboarder for a separate inji cluster
    Signed-off-by: Rakshithb1 <rakshit.b@technoforte.co.in>

--- partner-onboarder/copy_cm.sh
+++ partner-onboarder/copy_cm.sh
@@ -6,5 +6,5 @@ COPY_UTIL=./copy_cm_func.sh
 DST_NS=injiweb
 
 $COPY_UTIL configmap global default $DST_NS
-$COPY_UTIL configmap keycloak-env-vars keycloak $DST_NS
-$COPY_UTIL configmap keycloak-host keycloak $DST_NS
+$COPY_UTIL configmap keycloak-env-vars keycloak $DST_NS || { echo "Ignore the error if Keycloak values have been added to values.yaml."; true; }
+$COPY_UTIL configmap keycloak-host keycloak $DST_NS || { echo "Ignore the error if Keycloak values have been added to values.yaml."; true; }
--- partner-onboarder/copy_secrets.sh
+++ partner-onboarder/copy_secrets.sh
@@ -5,6 +5,6 @@
 COPY_UTIL=./copy_cm_func.sh
 DST_NS=injiweb
 
-$COPY_UTIL secret s3 s3 $DST_NS
-$COPY_UTIL secret keycloak keycloak $DST_NS
-$COPY_UTIL secret keycloak-client-secrets keycloak $DST_NS
+#$COPY_UTIL secret s3 s3 $DST_NS
+$COPY_UTIL secret keycloak keycloak $DST_NS || { echo "Ignore the error if Keycloak values have been added to values.yaml."; true; }
+$COPY_UTIL secret keycloak-client-secrets keycloak $DST_NS || { echo "Ignore the error if Keycloak values have been added to values.yaml."; true; }
