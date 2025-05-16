693ea8c04b97fd4592221bb9abac6a992c462ebf
Nandeesh778 <144665029+Nandeesh778@users.noreply.github.com>
2025-05-16 17:37:06 +0530

    Add support for w3c data model 2.0  (#708)
    * Modified the mimoto code to support 2.0 data model.
    
    Signed-off-by: Nandeesh <nandeeshv0001@gmail.com>
    
    * Data model 2.0 support - deriving the context from the request.
    
    Signed-off-by: Nandeesh <nandeeshv0001@gmail.com>
    
    * fix: add fallback to VC v1 context and reverted the testcase change.
    
    Signed-off-by: Nandeesh <nandeeshv0001@gmail.com>
    
    * updated the io.mosip.testrig.apitest.commons dependency.
    
    Signed-off-by: Nandeesh <nandeeshv0001@gmail.com>
    
    * Added the unit tests with context generation scenarios.
    
    Signed-off-by: Nandeesh <nandeeshv0001@gmail.com>
    
    * Removed the print statements in the unit tests.
    
    Signed-off-by: Nandeesh <nandeeshv0001@gmail.com>
    
    * Removed the unwanted annotation at unittests and refactored the context creation code.
    
    Signed-off-by: Nandeesh <nandeeshv0001@gmail.com>
    
    ---------
    
    Signed-off-by: Nandeesh <nandeeshv0001@gmail.com>

--- src/main/java/io/mosip/mimoto/util/CredentialUtilService.java
+++ src/main/java/io/mosip/mimoto/util/CredentialUtilService.java
@@ -151,6 +151,11 @@ public class CredentialUtilService {
             byte[] privateKeyInBytes = encryptionDecryptionUtil.decryptWithAES(walletKey, proofSigningKey.get().getEncryptedSecretKey());
             jwt = JwtGeneratorUtil.generateJwtUsingDBKeys(algorithm, credentialIssuerWellKnownResponse.getCredentialIssuer(), issuerDTO.getClient_id(), accessToken, publicKeyBytes, privateKeyInBytes);
         }
+        List<String> credentialContext = credentialsSupportedResponse.getCredentialDefinition().getContext();
+
+        if (credentialContext == null || credentialContext.isEmpty()) {
+            credentialContext = List.of("https://www.w3.org/2018/credentials/v1");
+        }
 
         return VCCredentialRequest.builder()
                 .format(credentialsSupportedResponse.getFormat())
@@ -160,7 +165,7 @@ public class CredentialUtilService {
                         .build())
                 .credentialDefinition(VCCredentialDefinition.builder()
                         .type(credentialsSupportedResponse.getCredentialDefinition().getType())
-                        .context(List.of("https://www.w3.org/2018/credentials/v1"))
+                        .context(credentialContext)
                         .build())
                 .build();
     }
