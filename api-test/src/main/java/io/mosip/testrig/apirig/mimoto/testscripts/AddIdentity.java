fecc3fee00309c53af9086d2e7ecc33d69d313af
SradhaMohanty5899 <mohantysradha10@gmail.com>
2025-05-19 13:57:31 +0530

    MOSIP-40258
    Signed-off-by: SradhaMohanty5899 <mohantysradha10@gmail.com>

--- api-test/src/main/java/io/mosip/testrig/apirig/mimoto/testscripts/AddIdentity.java
+++ api-test/src/main/java/io/mosip/testrig/apirig/mimoto/testscripts/AddIdentity.java
@@ -38,6 +38,7 @@ import io.mosip.testrig.apirig.utils.KernelAuthentication;
 import io.mosip.testrig.apirig.utils.OutputValidationUtil;
 import io.mosip.testrig.apirig.utils.ReportUtil;
 import io.mosip.testrig.apirig.utils.RestClient;
+import io.mosip.testrig.apirig.utils.SecurityXSSException;
 import io.restassured.response.Response;
 
 public class AddIdentity extends MimotoUtil implements ITest {
@@ -84,7 +85,7 @@ public class AddIdentity extends MimotoUtil implements ITest {
 	 * @throws AdminTestException
 	 */
 	@Test(dataProvider = "testcaselist")
-	public void test(TestCaseDTO testCaseDTO) throws AuthenticationTestException, AdminTestException {
+	public void test(TestCaseDTO testCaseDTO) throws AuthenticationTestException, AdminTestException, SecurityXSSException {
 		testCaseName = testCaseDTO.getTestCaseName();
 		if (HealthChecker.signalTerminateExecution) {
 			throw new SkipException(
