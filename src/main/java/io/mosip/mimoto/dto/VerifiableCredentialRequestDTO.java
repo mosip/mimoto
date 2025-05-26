cbc50851c6a059cf1ec927be1a3cef38d02ddcc1
KiruthikaJeyashankar <kiruthikavjshankar@gmail.com>
2025-05-26 22:39:23 +0530

    [INJIWEB-1541] add scheme description for VerifiableCredentialRequestDTO
    Signed-off-by: KiruthikaJeyashankar <kiruthikavjshankar@gmail.com>

--- src/main/java/io/mosip/mimoto/dto/VerifiableCredentialRequestDTO.java
+++ src/main/java/io/mosip/mimoto/dto/VerifiableCredentialRequestDTO.java
@@ -1,5 +1,6 @@
 package io.mosip.mimoto.dto;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.NotBlank;
 import lombok.Data;
 import lombok.Getter;
@@ -9,13 +10,23 @@ import lombok.Setter;
 @Getter
 @Setter
 public class VerifiableCredentialRequestDTO {
-    //TODO: Add schema definition
+    @Schema(description = "The unique identifier of the issuer")
     @NotBlank(message = "Missing required parameters: issuerId")
     String issuer;
+
+    @Schema(description = "The unique identifier of the credential type from the issuer well-known configuration")
     @NotBlank(message = "Missing required parameters: credentialConfigurationId")
     String credentialConfigurationId;
+
+    @Schema(description = "The authorization code received from the authorization server")
     String code;
+
+    @Schema(description = "The grant type for the authorization request")
     String grantType;
+
+    @Schema(description = "The redirect URI for the authorization request")
     String redirectUri;
+
+    @Schema(description = "The code verifier used for PKCE (Proof Key for Code Exchange)")
     String codeVerifier;
 }
