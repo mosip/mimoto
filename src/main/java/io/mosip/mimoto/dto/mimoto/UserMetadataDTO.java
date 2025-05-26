83a78846ce1ae54780de42822aba308817676573
KiruthikaJeyashankar <kiruthikavjshankar@gmail.com>
2025-05-26 22:38:55 +0530

    [INJIWEB-1541] modify api response to be in camelCase for getUserInfo API
    Signed-off-by: KiruthikaJeyashankar <kiruthikavjshankar@gmail.com>

--- src/main/java/io/mosip/mimoto/dto/mimoto/UserMetadataDTO.java
+++ src/main/java/io/mosip/mimoto/dto/mimoto/UserMetadataDTO.java
@@ -13,15 +13,12 @@ public class UserMetadataDTO implements Serializable {
 
     private static final long serialVersionUID = 1L;
 
-    @JsonProperty("display_name")
     @Schema(description = "Display name of the user provided by the Identity Provider")
     private String displayName;
 
-    @JsonProperty("profile_picture_url")
     @Schema(description = "Profile picture of the user provided by the Identity Provider")
     private String profilePictureUrl;
 
-    @JsonProperty("email")
     @Schema(description = "Email of the user provided by the Identity Provider")
     private String email;
 }
