3e139ec89c30c656a68234a5b10186e94651434f
Mahesh-Binayak <76687012+Mahesh-Binayak@users.noreply.github.com>
2025-06-03 11:12:05 +0530

    [MOSIP-39631]added options for variable namespace. (#731)
    Signed-off-by: Mahesh.Binayak <mahesh.binyak@technoforte.o>

--- partner-onboarder/copy_cm.sh
+++ partner-onboarder/copy_cm.sh
@@ -3,7 +3,7 @@
 # DST_NS: Destination namespace 
 
 COPY_UTIL=./copy_cm_func.sh
-DST_NS=injiweb
+DST_NS=$( printenv NS )
 
 $COPY_UTIL configmap inji-stack-config default $DST_NS
 $COPY_UTIL configmap keycloak-env-vars keycloak $DST_NS || { echo "Ignore this error if Keycloak is deployed externally and its details have been added to values.yaml. If not, please update values.yaml as per the instructions in README.md."; true; }
