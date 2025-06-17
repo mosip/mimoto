613ef946ca06da75b0f4325eeadd19fffc5a94b4
Gurpreet41082 <gurpreet.kaur@thoughtworks.com>
2025-06-17 13:25:57 +0530

    injiweb-1519 fixing mosip download credential
    Signed-off-by: Gurpreet41082 <gurpreet.kaur@thoughtworks.com>

--- src/main/java/io/mosip/mimoto/util/CredentialUtilService.java
+++ src/main/java/io/mosip/mimoto/util/CredentialUtilService.java
@@ -366,7 +366,7 @@ public class CredentialUtilService {
 
             // Find credentials supported response for the credential type
             CredentialsSupportedResponse credentialsSupportedResponse = issuerConfig.getCredentialsSupportedResponse();
-            if (credentialsSupportedResponse == null || !credentialsSupportedResponse.getCredentialDefinition().getType().contains(credentialMetadata.getCredentialType())) {
+            if (credentialsSupportedResponse == null || !credentialsSupportedResponse.getCredentialDefinition().getType().containsAll(vcCredentialResponse.getCredential().getType())) {
                 log.error("Credentials supported response not found for credentialType: {}", credentialMetadata.getCredentialType());
                 throw new CredentialProcessingException(CREDENTIAL_FETCH_EXCEPTION.getErrorCode(), "Invalid credential type configuration");
             }
