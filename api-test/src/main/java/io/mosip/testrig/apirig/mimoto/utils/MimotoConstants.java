34048fb74b47f4c023aa5f1b712ee34f924052d3
SradhaMohanty5899 <mohantysradha10@gmail.com>
2025-06-25 15:43:41 +0530

    Removed isCaptchaEnabled() and getValueFromEsignetActuator() methods after moving to commons util
    Signed-off-by: SradhaMohanty5899 <mohantysradha10@gmail.com>

--- api-test/src/main/java/io/mosip/testrig/apirig/mimoto/utils/MimotoConstants.java
+++ api-test/src/main/java/io/mosip/testrig/apirig/mimoto/utils/MimotoConstants.java
@@ -5,28 +5,5 @@ public class MimotoConstants {
 	public static final String SUNBIRD_INSURANCE_AUTH_FACTOR_TYPE = "KBI";
 	
 	public static final String SUNBIRD_INSURANCE_AUTH_FACTOR_TYPE_STRING = "sunbirdInsuranceAuthFactorType";
-	
-    public static final String ACTIVE_PROFILES = "activeProfiles";
-	
-	public static final String ESIGNET_ACTUATOR_ENDPOINT_KEYWORD = "actuatorEsignetEndpoint";
-	
-	public static final String ESIGNET_BASE_URL = MimotoConfigManager.getEsignetBaseUrl();
-	
-	public static final String ESIGNET_ACTUATOR_URL = ESIGNET_BASE_URL
-			+ MimotoConfigManager.getproperty(ESIGNET_ACTUATOR_ENDPOINT_KEYWORD);
-	
-	public static final String SYSTEM_ENV_SECTION = "systemEnvironment";
-	
-    public static final String CLASS_PATH_APPLICATION_PROPERTIES = "classpath:/application.properties";
-	
-	public static final String MOSIP_ESIGNET_CAPTCHA_REQUIRED = "mosip.esignet.captcha.required";
-	
-	public static final String CLASS_PATH_APPLICATION_DEFAULT_PROPERTIES = "classpath:/application-default.properties";
-	
-	public static final String DEFAULT_STRING = "default";
-	
-	public static final String MOSIP_CONFIG_APPLICATION_HYPHEN_STRING = "mosip-config/application-";
-	
-	public static final String DOT_PROPERTIES_STRING = ".properties";
 
 }
