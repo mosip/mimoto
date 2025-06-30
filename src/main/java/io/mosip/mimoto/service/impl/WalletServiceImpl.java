2708b4d18be7a8605ed8328685c6ef9f7795f762
Gurpreet41082 <gurpreet.kaur@thoughtworks.com>
2025-06-30 19:43:16 +0530

    injiweb-1605 added the refactoring of WalletServiceImpl
    Signed-off-by: Gurpreet41082 <gurpreet.kaur@thoughtworks.com>

--- src/main/java/io/mosip/mimoto/service/impl/WalletServiceImpl.java
+++ src/main/java/io/mosip/mimoto/service/impl/WalletServiceImpl.java
@@ -62,14 +62,12 @@ public class WalletServiceImpl implements WalletService {
         validator.validateUserId(userId);
         validator.validateWalletPin(pin);
 
-        Wallet wallet = repository.findByUserIdAndId(userId, walletId)
-                .orElseThrow(getWalletNotFoundExceptionSupplier(userId, walletId));
-
-        String decryptedWalletKey = walletUtil.decryptWalletKey(wallet.getWalletKey(), pin);
-        httpSession.setAttribute(SessionKeys.WALLET_KEY, decryptedWalletKey);
-        httpSession.setAttribute(SessionKeys.WALLET_ID, walletId);
-
-        return new WalletResponseDto(walletId, wallet.getWalletMetadata().getName());
+        return repository.findByUserIdAndId(userId, walletId).map(wallet -> {
+            String decryptedWalletKey = walletUtil.decryptWalletKey(wallet.getWalletKey(), pin);
+            httpSession.setAttribute(SessionKeys.WALLET_KEY, decryptedWalletKey);
+            httpSession.setAttribute(SessionKeys.WALLET_ID, walletId);
+            return new WalletResponseDto(walletId, wallet.getWalletMetadata().getName());
+        }).orElseThrow(getWalletNotFoundExceptionSupplier(userId, walletId));
     }
 
     @Override
@@ -79,9 +77,7 @@ public class WalletServiceImpl implements WalletService {
 
         log.info("Retrieving wallets for user: {}", userId);
 
-        List<Wallet> wallets = repository.findWalletByUserId(userId);
-        return wallets.stream()
-                .map(wallet -> WalletResponseDto.builder()
+        return repository.findWalletByUserId(userId).stream().map(wallet -> WalletResponseDto.builder()
                         .walletId(wallet.getId())
                         .walletName(wallet.getWalletMetadata().getName())
                         .build())
