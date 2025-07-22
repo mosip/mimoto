d3336d97e12aa96a61ea258cc63ffc16ca2f0357
PuBHARGAVI <46226958+PuBHARGAVI@users.noreply.github.com>
2025-07-22 16:12:27 +0530

    [INJIWEB-1662] ingore the unknown properties in WalletMetadata class to fix the unknown property issue happening related to passcode retry properties of wallet stored in DB
    Signed-off-by: PuBHARGAVI <46226958+PuBHARGAVI@users.noreply.github.com>

--- src/main/java/io/mosip/mimoto/model/WalletMetadata.java
+++ src/main/java/io/mosip/mimoto/model/WalletMetadata.java
@@ -1,8 +1,10 @@
 package io.mosip.mimoto.model;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import lombok.Data;
 
 @Data
+@JsonIgnoreProperties(ignoreUnknown = true)
 public class WalletMetadata {
 
     private String encryptionAlgo;
