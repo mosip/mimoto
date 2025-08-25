07c9d9aa29a262c53ee7412a2170e2e36cb83d4d
abhip2565 <paul.apaul.abhishek.AP@gmail.com>
2025-08-25 16:32:07 +0530

    [INJIMOB-3571] [INJIMOB-3572] add jwks_uri and allowUnsignedRequest attributes to VerifierDTO
    [INJIMOB-3455] add client metadata in trusted verifier DTO (#848)
    
    Signed-off-by: Abhishek Paul <paul.apaul.abhishek.ap@gmail.com>
    
    [INJIMOB-3571] add: jwks_uri to VerifierDTO (#936)
    
    * [INJIMOB-3571] add: VerifierDTO include jwks_uri
    
    - VerifierDTO does not include client_metadata
    
    Signed-off-by: KiruthikaJeyashankar <kiruthikavjshankar@gmail.com>
    
    * [INJIMOB-3572] add: allowUnsignedRequest config to  VerifierDTO
    
    allowUnsignedRequest is a configuration to specify if this particular Verifier can accept signed authorization request or not. The default is false meaning Only Signed Requests are allowed.
    
    Signed-off-by: KiruthikaJeyashankar <kiruthikavjshankar@gmail.com>
    
    ---------
    
    Signed-off-by: KiruthikaJeyashankar <kiruthikavjshankar@gmail.com>
    
    [INJIMOB-3572] refactor: update verifiers config in trusted-verifiers-config.json (#939)
    
    * [INJIMOB-3572] refactor: update trusted verifiers as per verifier config
    
    Signed-off-by: KiruthikaJeyashankar <kiruthikavjshankar@gmail.com>
    
    * [INJIMOB-3572] refactor: modify test names as per test
    
    Signed-off-by: KiruthikaJeyashankar <kiruthikavjshankar@gmail.com>
    
    ---------
    
    Signed-off-by: KiruthikaJeyashankar <kiruthikavjshankar@gmail.com>
    Signed-off-by: swatigoel <meet2swati@gmail.com>

--- src/test/java/io/mosip/mimoto/controller/VerifiersControllerTest.java
+++ src/test/java/io/mosip/mimoto/controller/VerifiersControllerTest.java
@@ -18,6 +18,8 @@ import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
 import java.util.Collections;
+import java.util.HashMap;
+import java.util.Map;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
@@ -70,5 +72,52 @@ public class VerifiersControllerTest {
                 .andExpect(status().isBadRequest())
                 .andExpect(jsonPath("$.response.verifiers").isArray())
                 .andExpect(jsonPath("$.response.verifiers").isEmpty());
+    }
+
+    @Test
+    public void shouldIncludeAllProvidedPropertiesInTrustedVerifiers() throws Exception {
+        Map<String, Object> metadata = new HashMap<>();
+        metadata.put("client_name", "Test-Verifier");
+
+        VerifierDTO verifierDTO = VerifierDTO.builder()
+                .clientId("test-clientId")
+                .redirectUris(Collections.singletonList("https://test-redirectUri"))
+                .responseUris(Collections.singletonList("https://test-responseUri"))
+                .jwksUri("https://test/.well-known/jwks.json")
+                .allowUnsignedRequest(true)
+                .build();
+
+        VerifiersDTO trustedVerifiers = VerifiersDTO.builder()
+                .verifiers(Collections.singletonList(verifierDTO)).build();
+
+        Mockito.when(verifierService.getTrustedVerifiers())
+                .thenReturn(trustedVerifiers);
+
+        mockMvc.perform(get("/verifiers").accept(MediaType.APPLICATION_JSON_VALUE))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.response.verifiers[0].allow_unsigned_request").value(true))
+                .andExpect(jsonPath("$.response.verifiers[0].client_id").value("test-clientId"))
+                .andExpect(jsonPath("$.response.verifiers[0].jwks_uri").value("https://test/.well-known/jwks.json"));
+    }
+
+    @Test
+    public void shouldReturnDefaultValuesWhenOptionalFieldsAreNotProvided() throws Exception {
+        // Not providing jwks_uri and allow_unsigned_request in VerifierDTO
+        VerifierDTO verifierDTO = VerifierDTO.builder()
+                .clientId("test-clientId")
+                .redirectUris(Collections.singletonList("https://test-redirectUri"))
+                .responseUris(Collections.singletonList("https://test-responseUri"))
+                .build();
+
+        VerifiersDTO trustedVerifiers = VerifiersDTO.builder()
+                .verifiers(Collections.singletonList(verifierDTO)).build();
+
+        Mockito.when(verifierService.getTrustedVerifiers())
+                .thenReturn(trustedVerifiers);
+
+        mockMvc.perform(get("/verifiers").accept(MediaType.APPLICATION_JSON_VALUE))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.response.verifiers[0].allow_unsigned_request").value(false)) // allow_unsigned_request is not passed means it should take default value false
+                .andExpect(jsonPath("$.response.verifiers[0].jwks_uri").doesNotExist()); // jwks_uri is not passed means it should not be present in trusted verifiers
     }
 }
