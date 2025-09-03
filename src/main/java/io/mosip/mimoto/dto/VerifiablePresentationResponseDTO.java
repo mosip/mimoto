1a1cabd90b042a6a254a7b1293aba2e68c976d0d
PuBHARGAVI <46226958+PuBHARGAVI@users.noreply.github.com>
2025-09-03 17:14:16 +0530

    [INJIWEB-1704] add serialized name for VerifiablePresentationVerifierDTO in response dto of VP
    Signed-off-by: PuBHARGAVI <46226958+PuBHARGAVI@users.noreply.github.com>

--- src/main/java/io/mosip/mimoto/dto/VerifiablePresentationResponseDTO.java
+++ src/main/java/io/mosip/mimoto/dto/VerifiablePresentationResponseDTO.java
@@ -1,5 +1,6 @@
 package io.mosip.mimoto.dto;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.AllArgsConstructor;
 import lombok.Data;
@@ -11,6 +12,7 @@ public class VerifiablePresentationResponseDTO {
     @Schema(description = "Unique identifier for the Verifiable Presentation")
     String presentationId;
 
+    @JsonProperty("verifier")
     @Schema(description = "Information about the Verifier who sent the Verifiable Presentation request")
     VerifiablePresentationVerifierDTO verifiablePresentationVerifierDTO;
 }
