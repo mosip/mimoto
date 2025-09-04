9e80bdc6605e2e6c3b3ee4de950e9edf7ee85203
PuBHARGAVI <46226958+PuBHARGAVI@users.noreply.github.com>
2025-09-04 16:17:58 +0530

    [INJIWEB-1704] handle error scenarios for wallet verifiable presentation endpoint and document these details for javadoc, stoplight and swagger
    Signed-off-by: PuBHARGAVI <46226958+PuBHARGAVI@users.noreply.github.com>

--- src/main/java/io/mosip/mimoto/service/OpenID4VPService.java
+++ src/main/java/io/mosip/mimoto/service/OpenID4VPService.java
@@ -2,10 +2,11 @@ package io.mosip.mimoto.service;
 
 import io.mosip.mimoto.dto.VerifiablePresentationResponseDTO;
 import io.mosip.mimoto.exception.ApiNotAccessibleException;
+import io.mosip.openID4VP.exceptions.OpenID4VPExceptions;
 
 import java.io.IOException;
 
 public interface OpenID4VPService {
-    VerifiablePresentationResponseDTO handleVPAuthorizationRequest(String urlEncodedVPAuthorizationRequest, String walletId) throws ApiNotAccessibleException, IOException;
+    VerifiablePresentationResponseDTO handleVPAuthorizationRequest(String urlEncodedVPAuthorizationRequest, String walletId) throws ApiNotAccessibleException, IOException, OpenID4VPExceptions;
 }
 
