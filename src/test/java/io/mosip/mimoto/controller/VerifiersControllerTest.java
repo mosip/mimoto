ea659b5f5b418cd47f68f9141da6535baacab12d
KiruthikaJeyashankar <kiruthikavjshankar@gmail.com>
2025-10-03 15:10:57 +0530

    [INJIMOB-3572] refactor: update verifiers config in trusted-verifiers-config.json (#939)
    * [INJIMOB-3572] refactor: update trusted verifiers as per verifier config
    
    Signed-off-by: KiruthikaJeyashankar <kiruthikavjshankar@gmail.com>
    
    * [INJIMOB-3572] refactor: modify test names as per test
    
    Signed-off-by: KiruthikaJeyashankar <kiruthikavjshankar@gmail.com>
    
    ---------
    
    Signed-off-by: KiruthikaJeyashankar <kiruthikavjshankar@gmail.com>

--- src/test/java/io/mosip/mimoto/controller/VerifiersControllerTest.java
+++ src/test/java/io/mosip/mimoto/controller/VerifiersControllerTest.java
@@ -75,7 +75,7 @@ public class VerifiersControllerTest {
     }
 
     @Test
-    public void shouldIncludeClientMetadataWhenClientMetadataIsPresentInTrustedVerifiers() throws Exception {
+    public void shouldIncludeAllProvidedPropertiesInTrustedVerifiers() throws Exception {
         Map<String, Object> metadata = new HashMap<>();
         metadata.put("client_name", "Test-Verifier");
 
@@ -101,12 +101,12 @@ public class VerifiersControllerTest {
     }
 
     @Test
-    public void shouldReturnEmptyClientMetadataWhenClientMetadataIsEmptyInTrustedVerifiers() throws Exception {
+    public void shouldReturnDefaultValuesWhenOptionalFieldsAreNotProvided() throws Exception {
+        // Not providing jwks_uri and allow_unsigned_request in VerifierDTO
         VerifierDTO verifierDTO = VerifierDTO.builder()
                 .clientId("test-clientId")
                 .redirectUris(Collections.singletonList("https://test-redirectUri"))
                 .responseUris(Collections.singletonList("https://test-responseUri"))
-                .jwksUri("https://test/.well-known/jwks.json")
                 .build();
 
         VerifiersDTO trustedVerifiers = VerifiersDTO.builder()
@@ -117,10 +117,7 @@ public class VerifiersControllerTest {
 
         mockMvc.perform(get("/verifiers").accept(MediaType.APPLICATION_JSON_VALUE))
                 .andExpect(status().isOk())
-                .andExpect(jsonPath("$.response.verifiers[0].allow_unsigned_request").value(false))
-                .andExpect(jsonPath("$.response.verifiers[0].jwks_uri").isString())
-                .andExpect(jsonPath("$.response.verifiers[0].jwks_uri").value("https://test/.well-known/jwks.json"));
+                .andExpect(jsonPath("$.response.verifiers[0].allow_unsigned_request").value(false)) // allow_unsigned_request is not passed means it should take default value false
+                .andExpect(jsonPath("$.response.verifiers[0].jwks_uri").doesNotExist()); // jwks_uri is not passed means it should not be present in trusted verifiers
     }
-
-
 }
